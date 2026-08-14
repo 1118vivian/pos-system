614dc841c8d72c4603ed4e6e9850d36245495dcb
1118vivian <yajuchi@gmail.com>
2026-08-14 18:19:28 +0800

    保存雲端設定並修正管理登入

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 SHEET_NAME = "POS系統資料庫"
 CREDENTIALS_FILE = "credentials.json"
-APP_PASSWORD = os.environ.get("POS_APP_PASSWORD", "1234")
+APP_PASSWORD = os.environ.get("POS_APP_PASSWORD", "1234").strip()
 SECRET_KEY = os.environ.get("POS_SECRET_KEY", "pos-system-change-me")
